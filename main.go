1e4e4ed780868b5889ac036ee88740872f6e670c
focta <focke.wulf7234@gmail.com>
2024-06-10 18:35:12 +0900

    テスト関連の記事を参考にしながらginのテストを実行してみた

--- main.go
+++ main.go
@@ -9,21 +9,24 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type User struct {
+	Name string `json:"name" binding:"required"`
+}
+
 func main() {
 	fmt.Println("Hello World")
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r := router()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
 
 	// 関数を外出しする手法
 	r.GET("/quickstart", quickStart)
 
 	// ミドルウェアの利用
-	
 	ua := ""
 	r.Use(func(c *gin.Context) {
 		ua = c.GetHeader("User-Agent")
@@ -44,4 +47,25 @@ func quickStart(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "quickStart",
 	})
+}
+
+func router() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/ps", func(c *gin.Context) {
+		var u User
+		if err := c.BindJSON(&u); err != nil {
+			// const StatusUnauthorized untyped int = 401
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "error"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"msg": u})
+	})
+
+	return r
 }
